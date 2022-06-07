2ade081aa087b6dd32492eadbb3c36ba43ba87bc
perkles <marcelo.p.mendes2019@gmail.com>
2022-06-07 20:47:33 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,83 @@
+
+function red(){
+    limpar();
+    document.querySelector("#container").classList.add("red");
+}
+function blue(){
+    limpar();
+    document.querySelector("#container").classList.add("blue");
+}
+function orange(){
+    limpar();
+    document.querySelector("#container").classList.add("orange");
+}
+function green(){
+    limpar();
+    document.querySelector("#container").classList.add("green");
+}
+function violet(){
+    limpar();
+    document.querySelector("#container").classList.add("violet");
+}
+function orange(){
+    limpar();
+    document.querySelector("#container").classList.add("orange");
+}
+function purple(){
+    limpar();
+    document.querySelector("#container").classList.add("purple");
+}
+function pink(){
+    limpar();
+    document.querySelector("#container").classList.add("pink");
+}
+function crimson(){
+    limpar();
+    document.querySelector("#container").classList.add("crimson");
+}
+function aqua(){
+    limpar();
+    document.querySelector("#container").classList.add("aqua");
+}
+function brown(){
+    limpar();
+    document.querySelector("#container").classList.add("brown");
+}
+function tomato(){
+    limpar();
+    document.querySelector("#container").classList.add("tomato");
+}
+function black(){
+    limpar();
+    document.querySelector("#container").classList.add("black");
+}
+function grey(){
+    limpar();
+    document.querySelector("#container").classList.add("grey");
+}
+function fuchsia(){
+    limpar();
+    document.querySelector("#container").classList.add("fuchsia");
+}
+function yellow(){
+    limpar();
+    document.querySelector("#container").classList.add("yellow");
+}
+function limpar(){
+    document.querySelector('#container').classList.remove("red")
+    document.querySelector('#container').classList.remove("blue")
+    document.querySelector('#container').classList.remove("violet")
+    document.querySelector('#container').classList.remove("orange")
+    document.querySelector('#container').classList.remove("green")
+    document.querySelector('#container').classList.remove("purple")
+    document.querySelector('#container').classList.remove("pink")
+    document.querySelector('#container').classList.remove("crimson")
+    document.querySelector('#container').classList.remove("aqua")
+    document.querySelector('#container').classList.remove("brown")
+    document.querySelector('#container').classList.remove("tomato")
+    document.querySelector('#container').classList.remove("black")
+    document.querySelector('#container').classList.remove("grey")
+    document.querySelector('#container').classList.remove("fuchsia")
+    document.querySelector('#container').classList.remove("yellow")
+
+}
